281c733ee165e0a59c33695c8acf4e87300180e1
Sherryll Elliott <sherryllelliott@gmail.com>
2020-12-22 16:20:53 +0000

    more styling on skills

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import splashRight from '../styles/splashRight.png'
 import splashLeft from '../styles/splashLeft.png'
-// import section from '../styles/section.png'
+
 const About = () => {
   return <main className="about">
 
     <h1> About Me </h1>
     <div className="aboutText">
       <img className="splash" src={splashLeft}></img>
-      {/* <img className="sectionImage" src={section}></img> */}
-
       <p className="aboutParagraph">
         For the 15 years or so (read the glorious era of my 20s where my knees didn’t complain)
       I was teaching Physics to 11-18 year olds.<br></br><br></br>
@@ -18,6 +16,7 @@ const About = () => {
       </p>
       <img className="splash" src={splashRight}></img>
     </div>
+    
   </main>
 }
 
